d77f5a2162804afd65bbb175aa956695e7f83e48
Pavel Mezencev <mezencev24@mail.ru>
2022-04-21 22:58:31 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const userName = 'Pavel';
+
+console.log(userName);
+const person = {
+  name: 'Pavel',
+  friends: [
+    { name: 'Ivan', age: 33 },
+    { name: 'Mihail', age: 36 },
+    { name: 'Ivan', age: 34 },
+  ],
+};
+
+console.log(person);
